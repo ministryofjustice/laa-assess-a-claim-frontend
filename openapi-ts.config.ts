9b49c3d57dee3f58df560ed17a8c5e4e97ee39e2
liampan <liam.oldfield@justice.gov.uk>
2026-04-08 11:37:01 +0100

    CLAS-262 | generate on build from file

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-  input: 'http://localhost:8080/v3/api-docs',
+  input: 'openapi/claim/v3/api-docs.json',
   output: 'src/generated/claim-api',
   plugins: ['@hey-api/client-axios'],
 });
